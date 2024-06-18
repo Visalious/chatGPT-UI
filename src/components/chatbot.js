297c3c2e0635197d5e73bbde0988012bc12d2ae8
Visalious <wisalwajid786@gmail.com>
2024-06-18 17:53:44 +0500

    sidebar

--- src/components/chatbot.js
+++ src/components/chatbot.js
@@ -1,8 +1,6 @@
-import React from 'react';
-import { Button, Dropdown, Menu, Input, Row, Col } from 'antd';
-import {
-  DownOutlined,
-} from '@ant-design/icons';
+import React, { useState } from 'react';
+import { Dropdown, Menu, Row, Col } from 'antd';
+import { DownOutlined } from '@ant-design/icons';
 import 'tailwindcss/tailwind.css';
 
 const menu = (
@@ -17,10 +15,17 @@ const menu = (
 );
 
 const ChatBot = () => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
   return (
     <Row className="bg-[#fff] min-h-screen flex">
       <Col
-        className="bg-[#f9f9f9] relative p-2 flex flex-col"
+        className={`bg-[#f9f9f9] p-2 flex flex-col ${
+          isSidebarOpen ? '' : 'hidden'
+        }`}
         xxl={4}
         xl={4}
         lg={4}
@@ -38,7 +43,6 @@ const ChatBot = () => {
                 width="20"
                 height="20"
                 viewBox="0,0,256,256"
-                className="cursor-pointer"
               >
                 <g
                   fill="#131313"
@@ -76,6 +80,7 @@ const ChatBot = () => {
             viewBox="0 0 24 24"
             class="icon-xl-heavy"
             color="#000"
+            className="cursor-pointer"
           >
             <path d="M15.673 3.913a3.121 3.121 0 1 1 4.414 4.414l-5.937 5.937a5 5 0 0 1-2.828 1.415l-2.18.31a1 1 0 0 1-1.132-1.13l.311-2.18A5 5 0 0 1 9.736 9.85zm3 1.414a1.12 1.12 0 0 0-1.586 0l-5.937 5.937a3 3 0 0 0-.849 1.697l-.123.86.86-.122a3 3 0 0 0 1.698-.849l5.937-5.937a1.12 1.12 0 0 0 0-1.586M11 4A1 1 0 0 1 10 5c-.998 0-1.702.008-2.253.06-.54.052-.862.141-1.109.267a3 3 0 0 0-1.311 1.311c-.134.263-.226.611-.276 1.216C5.001 8.471 5 9.264 5 10.4v3.2c0 1.137 0 1.929.051 2.546.05.605.142.953.276 1.216a3 3 0 0 0 1.311 1.311c.263.134.611.226 1.216.276.617.05 1.41.051 2.546.051h3.2c1.137 0 1.929 0 2.546-.051.605-.05.953-.142 1.216-.276a3 3 0 0 0 1.311-1.311c.126-.247.215-.569.266-1.108.053-.552.06-1.256.06-2.255a1 1 0 1 1 2 .002c0 .978-.006 1.78-.069 2.442-.064.673-.192 1.27-.475 1.827a5 5 0 0 1-2.185 2.185c-.592.302-1.232.428-1.961.487C15.6 21 14.727 21 13.643 21h-3.286c-1.084 0-1.958 0-2.666-.058-.728-.06-1.369-.185-1.96-.487a5 5 0 0 1-2.186-2.185c-.302-.592-.428-1.233-.487-1.961C3 15.6 3 14.727 3 13.643v-3.286c0-1.084 0-1.958.058-2.666.06-.729.185-1.369.487-1.961A5 5 0 0 1 5.73 3.545c.556-.284 1.154-.411 1.827-.475C8.22 3.007 9.021 3 10 3A1 1 0 0 1 11 4"></path>
           </svg>
@@ -92,17 +97,21 @@ const ChatBot = () => {
             Log In
           </button>
         </div>
-        <span className="w-[5px] !cursor-pointer h-6 bg-[#cccbcd] absolute -right-4 top-[50%] rounded-md"></span>
       </Col>
       <Col
-        className="flex flex-col "
-        xxl={20}
-        xl={20}
-        lg={20}
-        md={20}
-        sm={20}
-        xs={20}
+        className="flex flex-col relative"
+        xxl={isSidebarOpen ? 20 : 24}
+        xl={isSidebarOpen ? 20 : 24}
+        lg={isSidebarOpen ? 20 : 24}
+        md={isSidebarOpen ? 20 : 24}
+        sm={isSidebarOpen ? 20 : 24}
+        xs={isSidebarOpen ? 20 : 24}
       >
+        {' '}
+        <span
+          className="w-[5px] !cursor-pointer h-6 bg-[#cccbcd] absolute left-4 top-[50%] rounded-md z-50"
+          onClick={toggleSidebar}
+        ></span>
         <div className="flex items-center justify-between p-4 pt-2">
           <div className="flex items-center space-x-4">
             <Dropdown overlay={menu} trigger={['click']}>
@@ -117,7 +126,6 @@ const ChatBot = () => {
             </Dropdown>
           </div>
         </div>
-
         <div className="h-[90vh] flex flex-col justify- items-center">
           <div className="text-center flex-grow mt-6 flex items-center justify-center   flex-col">
             <div className="rounded-[50%] border-[2px] w-fit border-[#ecedeb] p-1">
